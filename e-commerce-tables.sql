066469e1b148e929f8773a2e581d82bc54cef842
Sodikcp <paramesh.rah@gmail.com>
2024-01-22 14:57:53 +0700

    feat: complete HH PostgerSQL

--- e-commerce-tables.sql
+++ e-commerce-tables.sql
@@ -1,1 +1,24 @@
 -- Start writing code here
+CREATE TABLE users (
+  	user_id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  	username VARCHAR ( 255 ) UNIQUE NOT NULL,
+	email VARCHAR(255) UNIQUE NOT NULL,
+	password VARCHAR ( 255 ) NOT NULL,
+  	created_at TIMESTAMPTZ NOT NULL)
+
+CREATE TABLE products (
+  	product_id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY UNIQUE,
+  	name VARCHAR ( 255 ) NOT NULL,
+	description text ,
+	price NUMERIC(10,2) NOT NULL,
+  	created_at TIMESTAMPTZ NOT NULL UNIQUE)
+
+CREATE TABLE categories (
+  	category_id  INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY UNIQUE,
+  	name VARCHAR ( 255 ) NOT NULL UNIQUE,
+  	created_at TIMESTAMPTZ NOT NULL UNIQUE)
+
+CREATE TABLE product_categories (
+  	product_category_id  INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY UNIQUE,
+  	product_id INT REFERENCES products(product_id) ON DELETE CASCADE UNIQUE NOT NULL,
+  	category_id INT REFERENCES categories(category_id) ON DELETE CASCADE NOT NULL UNIQUE)
